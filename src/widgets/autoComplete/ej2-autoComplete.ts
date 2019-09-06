48c51bf8e0ebcbb1e6e7ff85169306bbc7b7ce1d
Michael Stankovich <mike.stankovich@outlook.com>
2019-09-06 11:02:23 -0400

    fixed path

--- src/widgets/autoComplete/ej2-autoComplete.ts
+++ src/widgets/autoComplete/ej2-autoComplete.ts
@@ -1,6 +1,6 @@
 import { SyncfusionWrapper } from "../../common/syncfusionWrapper";
 import { AutoComplete, AutoCompleteModel } from "@syncfusion/ej2-dropdowns";
-import { generateBindables } from "utilities/decorator";
+import { generateBindables } from "../../utilities/decorator";
 
 @generateBindables("autoComplete")
 export class Ej2AutoComplete extends SyncfusionWrapper<AutoComplete, AutoCompleteModel> {
