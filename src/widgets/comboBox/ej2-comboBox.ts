a9cb9b1409e510dcabdf9fa760d8f4a6b1b570d5
Michael Stankovich <mike.stankovich@outlook.com>
2019-09-18 12:05:37 -0400

    fixed import

--- src/widgets/comboBox/ej2-comboBox.ts
+++ src/widgets/comboBox/ej2-comboBox.ts
@@ -1,6 +1,6 @@
 import { SyncfusionWrapper } from "../../common/syncfusionWrapper";
 import { ComboBox, ComboBoxModel } from "@syncfusion/ej2-dropdowns";
-import { generateBindables } from "utilities/decorator";
+import { generateBindables } from "../../utilities/decorator";
 
 @generateBindables("comboBox")
 export class Ej2ComboBox extends SyncfusionWrapper<ComboBox, ComboBoxModel> {
