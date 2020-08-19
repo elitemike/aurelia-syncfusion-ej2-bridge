9fdd7d9fd0dfe5ead7ce0b8aaeff5a78a0595276
Michael Stankovich <mike.stankovich@outlook.com>
2020-08-19 09:57:54 -0400

    Added id to make the syncfusion control work

--- src/widgets/fileManager/ej2-fileManager.ts
+++ src/widgets/fileManager/ej2-fileManager.ts
@@ -3,14 +3,17 @@ import { SyncfusionWrapper } from '../../common/syncfusionWrapper';
 import { generateBindables } from '../../utilities/decorator';
 import { inlineView, customElement } from "aurelia-framework"
 import { FileManager, FileManagerModel, Toolbar, NavigationPane, DetailsView, ContextMenu } from "@syncfusion/ej2-filemanager";
+import { v4 as uuidv4 } from "uuid";
 
 @generateBindables("fileManager")
-@inlineView(`<template><div element.ref="widgetElement"></div></template>`)
+@inlineView(`<template><div id.bind="id" element.ref="widgetElement"></div></template>`)
 @customElement('ej2-file-manager')
 export class Ej2FileManager extends SyncfusionWrapper<FileManager, FileManagerModel> {
   protected syncfusionWidgetType = FileManager;
+  public id = `fm-${uuidv4()}`;
 
   onBeforeWidgetInstantiation() {
+    // this.id = uuidv4();
     FileManager.Inject(Toolbar, NavigationPane, DetailsView, ContextMenu);
   }
   protected onWidgetCreated() {
